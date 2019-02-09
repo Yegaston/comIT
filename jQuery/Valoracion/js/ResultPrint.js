07345acdf701b22b171811044329e1fe1feb1026
Gaston Graciani <gastongraciani@gmail.com>
2019-02-08 23:11:28 -0300

    Add with some bugs

--- jQuery/Valoracion/js/ResultPrint.js
+++ jQuery/Valoracion/js/ResultPrint.js
@@ -5,14 +5,82 @@ function resultPrint(starAcum, cantVots) {
     const prom = starAcum / cantVots;
     console.log(prom);
 
-    if (prom > 0 && prom <= 0.5) {
-        $('#resultados').append(`
-        <img src="./resources/em_b.png" alt="Estrella completa">
+    if (prom == 0 && prom <= 0.5) {
+        $('#resultados').replaceWith(`
+        <img src="./resources/em_b.png" alt="Media Estrella">
        `);
-    }
-    if (prom > 0.5 && prom <= 1) {
-        $('#resultados').append(`
-        <img src="./resources/ec_b.png" alt="Estrella completa">
-       `);
+    } else if (prom > 0.5 && prom < 1) {
+
+        $('#resultados').replaceWith(`
+            <img src="./resources/ec_b.png" alt="Estrella completa">
+            `);
+        if (prom == 1 && prom < 1.5) {
+            $('#resultados').replaceWith(`
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/em_b.png" alt="Media Estrella">
+                `
+            );
+        }
+
+    } else if (prom > 1.5 && prom <= 2) {
+        $('#resultados').replaceWith(`
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/em_b.png" alt="Media Estrella">
+            `
+        );
+    } else if (prom == 2 && prom <= 2.5) {
+        $('#resultados').replaceWith(`
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/ec_b.png" alt="Estrella completa">   
+            `
+        );
+    } else if (prom > 2.5 && prom < 3) {
+        $('#resultados').replaceWith(`
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/em_b.png" alt="Media Estrella">   
+            `
+        );
+    } else if (prom == 3 && prom <= 3.5) {
+        $('#resultados').replaceWith(`
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+            `
+        );
+    } else if (prom > 3.5 && prom < 4) {
+        $('#resultados').replaceWith(`
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/em_b.png" alt="Media Estrella">
+            `
+        );
+    } else if (prom == 4 && prom <= 4.5) {
+        $('#resultados').replaceWith(`
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+            `
+        );
+    } else if (prom > 4.5 && prom < 5) {
+        $('#resultados').replaceWith(`
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/em_b.png" alt="Media Estrella">
+            `
+        );
+    } else if (prom == 5) {
+        $('#resultados').replaceWith(`
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+                <img src="./resources/ec_b.png" alt="Estrella completa">
+            `
+        );
     }
 }
